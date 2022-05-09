ff09d595a396489b62191c3296112ce8e691dbfc
Kamie Short <kamie.short@gmail.com>
2022-05-09 13:23:59 -0700

    added fetch functions, map and loader, characters list displaying on page

--- src/views/CharacterList.jsx
+++ src/views/CharacterList.jsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchCharacters } from '../services/fetch';
 
 export default function CharacterList() {
-  return <div>Character List will go right here</div>;
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      const data = await fetchCharacters();
+
+      setCharacters(data);
+      setLoading(false);
+    };
+    getCharacters();
+  }, []);
+
+  if (loading) return <div>Loading Characters...</div>;
+
+  return (
+    <>
+      <h1>Character List</h1>
+      {loading ? (
+        <p>Loading Characters...</p>
+      ) : (
+        <div>
+          {characters.map((character) => (
+            <div key={character.id}>
+              <h2>{character.name}</h2>
+              <p>{character.status}</p>
+              <img src={character.image} alt={`Image of ${character.name}`} />
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
